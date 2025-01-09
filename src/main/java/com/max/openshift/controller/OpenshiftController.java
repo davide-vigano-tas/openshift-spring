0203d8f0d79db0a8146d650489298eb936f78b90
EmulatorX <massimo.ghianda@gmail.com>
2025-01-09 13:19:25 +0100

    First commit

---
+++ src/main/java/com/max/openshift/controller/OpenshiftController.java
@@ -0,0 +1,15 @@
+package com.max.openshift.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class OpenshiftController {
+	@GetMapping("/")
+	public ModelAndView homePage() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("home");
+		return mv;
+	}
+}
